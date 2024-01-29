f815060473e2bb05c43dd6352ca85731803553b5
Ali <ali.ali.samadifard@gmail.com>
2024-01-30 01:47:33 +0330

    add sleep to show ensure locks work

--- test.c
+++ test.c
@@ -28,16 +28,18 @@ lock_t *lock;
 // Function to be executed by each thread
 void multiplyRow(void *arg) {
     int row = *((int *)arg);
-    printf(1,"current row== %d\n",row);
     for (int col = 0; col < MATRIX_SIZE; col++) {
         resultMatrix[row][col] = 0;
         for (int k = 0; k < MATRIX_SIZE; k++) {
-            lock_accuire(lock);
+            lock_acquire(lock);
             resultMatrix[row][col] += matrixA[row][k] * matrixB[k][col];
             lock_release(lock);
         }
+        sleep(10);
+        lock_acquire(lock);
+        printf(1,"tid== %d   and the result_matrix[%d][%d] has been calculated\n",row,row,col);
+        lock_release(lock);
     }
-
     exit();
 }
 
