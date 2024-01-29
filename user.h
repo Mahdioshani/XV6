e5348b83acbe5ec42c3f710bc48d6e29c88fe304
Mahdi Oshani <mahdi_oshani@comp.iust.ac.ir>
2024-01-30 00:18:34 +0330

    final changes in makefile and user.h and defs.h

--- user.h
+++ user.h
@@ -23,6 +23,8 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
+int clone(void (*function)(void*), void*, void*);
+int join(int, void**);
 
 // ulib.c
 int stat(const char*, struct stat*);
