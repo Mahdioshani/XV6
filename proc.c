92403935dde212a4eaa8e01814be049a04010fd3
Mahdi Oshani <mahdi_oshani@comp.iust.ac.ir>
2024-01-30 19:36:14 +0330

    bug fix

--- proc.c
+++ proc.c
@@ -351,25 +351,28 @@ scheduler(void)
             for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
                 if((p->state != RUNNABLE && p->thread_count==0)|| p->is_thread==1)
                     continue;
-                //cprintf("this process select:%d name: %s\n",p->pid,p->name);
-                // Switch to chosen process.  It is the process's job
-                // to release ptable.lock and then reacquire it
-                // before jumping back to us.
-                if(p->thread_count>0){
+                if(p->thread_count==0){
+                    c->proc = p;
+                    switchuvm(p);
+                    p->state = RUNNING;
+                    cprintf("pid :%d name: %s\n",p->pid,p->name);
+                    swtch(&(c->scheduler), p->context);
+                    switchkvm();
+
+                    // Process is done running for now.
+                    // It should have changed its p->state before coming back.
+                    c->proc = 0;
+                }
+                else{
                     if(p->turn==0){
                         if(p->state!=RUNNABLE){
                             p->turn=1;
-                            //cprintf("im here now");
                         }
                         c->proc = p;
                         switchuvm(p);
                         p->state = RUNNING;
-                        //cprintf("pid :%d name: %s\n",p->pid,p->name);
                         swtch(&(c->scheduler), p->context);
                         switchkvm();
-
-                        // Process is done running for now.
-                        // It should have changed its p->state before coming back.
                         c->proc = 0;
                     }if(p->turn!=0){
                         int index=0;
@@ -382,30 +385,14 @@ scheduler(void)
                                 c->proc = p2;
                                 switchuvm(p2);
                                 p2->state = RUNNING;
-                                //cprintf("pid :%d index of threead: %d name: %s\n",p->pid,index,p->name);
                                 swtch(&(c->scheduler), p2->context);
                                 switchkvm();
-
-                                // Process is done running for now.
-                                // It should have changed its p->state before coming back.
                                 c->proc = 0;
                                 break;
                             }
                         }
                     }
                     p->turn=(p->turn+1)%(p->thread_count+1);
-                }
-                else{
-                    c->proc = p;
-                    switchuvm(p);
-                    p->state = RUNNING;
-                    cprintf("pid :%d name: %s\n",p->pid,p->name);
-                    swtch(&(c->scheduler), p->context);
-                    switchkvm();
-
-                    // Process is done running for now.
-                    // It should have changed its p->state before coming back.
-                    c->proc = 0;
                 }
             }
             release(&ptable.lock);
